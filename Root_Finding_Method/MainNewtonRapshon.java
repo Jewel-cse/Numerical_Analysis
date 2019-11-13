933cb67cfe9e13df87c47742bb33cc472d833c51
Md Jewel <md.jewel.cse@gmail.com>
2019-11-13 17:21:49 +0600

    add MainNewtonRaphson and NewtonRaphson

---
+++ Root_Finding_Method/MainNewtonRapshon.java
@@ -0,0 +1,10 @@
+public class MainNewtonRapshon extends  NewtonRapson {
+		public static void main(String[] args) {
+		      double a;
+		      a = Math.random()*(50);
+		      System.out.println(" Initial value : " + a);
+		      double  root = NewRapMethod(a) ;
+		      System.out.println(" The Root is : " + root);
+	}
+
+}
